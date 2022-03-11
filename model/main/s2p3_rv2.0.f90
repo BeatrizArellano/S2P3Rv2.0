ee0df9b0712ad1c7128dc9d445f19ae8866a370c
BeatrizArellano <barellano@protonmail.com>
2022-03-11 17:28:19 +0000

    Output the Simpson-Hunter parameter

--- model/main/s2p3_rv2.0.f90
+++ model/main/s2p3_rv2.0.f90
@@ -1930,10 +1930,10 @@ timeloop: do itime=1,itotal                     ! <A NAME="START OF TIME LOOP">
       
       	!Output daily data
         !Format: Day, lon, lat, depth of the level, total depth, temperature
-        !chlorophyll, nitrogen?, PAR, uptake, growth          
+        !chlorophyll, nitrogen?, PAR,simpson-hunter parameter, uptake, growth          
         do i=1,N
           write(6,fmt="(i4,8f8.3)") iday,lon,lat,depth-(height(i)-dz/2.0),depth, &
-          temp_new(i),x_new(i),s_new(i), rad_mean(i)
+          temp_new(i),x_new(i),s_new(i), rad_mean(i),dlog10(depth/u3_mean)
         end do
       end if
 
